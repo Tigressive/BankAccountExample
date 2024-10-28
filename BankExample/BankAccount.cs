aeaf3f622af425c959e7a7f7abdb580ec4f0d99f
Ashton Atkinson <rascalcrafters@gmail.com>
2024-10-28 13:48:34 -0600

    Initial commit

---
+++ BankExample/BankAccount.cs
@@ -0,0 +1,26 @@
+﻿namespace BankExample;
+
+public class BankAccount
+{
+    private int balance;
+
+    public BankAccount(int balance)
+    {
+        this.balance = balance;
+    }
+
+    public void Deposit(int amount)
+    {
+        balance += amount;
+    }
+
+    public void Withdraw(int amount)
+    {
+        balance -= amount;
+    }
+
+    public int GetBalance()
+    {
+        return balance;
+    }
+}
---
+++ BankExample/Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+using BankExample;
+
+BankAccount bankAccount = new BankAccount(1000);
+
+
+bankAccount.Deposit(1000);
+bankAccount.Withdraw(500);
+
+Console.WriteLine(bankAccount.GetBalance());
